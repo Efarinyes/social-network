fabf58567bfe439eb154ff3281f1ccd4bf6789e3
Eduard Farinyes <efarinyes@gmail.com>
2025-02-06 12:45:28 +0100

    First Commit

---
+++ social/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+# Create your views here.
+def feed(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request, 'social/feed.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'Usuari {username} creat amb èxit')
+            return redirect('feed')
+    else:
+        form = UserRegisterForm()
+    context = {'form': form }
+
+    return render(request, 'social/register.html', context)
+
+def profile(request):
+    return render(request, 'social/profile.html')
