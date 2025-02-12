d924af315c8318556e7e69d57a282c064be5bcfe
Eduard Farinyes <efarinyes@gmail.com>
2025-02-12 19:16:18 +0100

    App finished

--- social/urls.py
+++ social/urls.py
@@ -8,10 +8,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.feed, name='feed'),
     path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='social/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='social/logout.html', http_method_names = ['get', 'post', 'options']), name='logout'),
     path('post/', views.post, name='post'),
+    path('follow/<str:username>/', views.follow, name='follow'),
+    path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
